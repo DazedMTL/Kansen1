ed6c9ecccea559b414620ed2e00aea8d8634030c
Dazed <dazedanon@gmail.com>
2023-10-24 13:44:19 -0500

    Force overwrite

--- patch.bat
+++ patch.bat
@@ -8,13 +8,13 @@ REM make a temporary directory if it doesn't exist
 if not exist %TMP% md %TMP%
 
 REM back up the data to a temporary location
-xcopy /E /I data %TMP%\data
+xcopy /E /I /Y data %TMP%\data
 
 REM unpack existing data
 kikiriki.exe -i data.xp3 -o data
 
 REM copy backup to overwrite new data
-xcopy /E /I %TMP%\data data
+xcopy /E /I /Y %TMP%\data data
 
 REM repack the data
 kikiriki.exe -c -i data -o data.xp3
