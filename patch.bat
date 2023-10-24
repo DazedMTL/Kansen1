9b4b2a8302955c9997a6a7b5e9670bab20e2c1a3
Dazed <dazedanon@gmail.com>
2023-10-24 13:30:34 -0500

    Remove data.xp3 and add kikriki and patch

---
+++ patch.bat
@@ -0,0 +1,23 @@
+@echo off
+
+REM specify the data and temporary directory
+set DATA=data.xp3
+set TMP=tmp
+
+REM make a temporary directory if it doesn't exist
+if not exist %TMP% md %TMP%
+
+REM back up the data to a temporary location
+xcopy /E /I data %TMP%\data
+
+REM unpack existing data
+kikiriki.exe -i data.xp3 -o data
+
+REM copy backup to overwrite new data
+xcopy /E /I %TMP%\data data
+
+REM repack the data
+kikiriki.exe -c -i data -o data.xp3
+
+REM delete the temporary directory
+rd /S /Q %TMP%
